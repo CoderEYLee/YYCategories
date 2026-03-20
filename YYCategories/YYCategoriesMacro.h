6d978976ff172c4a07376ab2cdc36621a7967f29
lieryang <lieryangios@126.com>
2026-03-20 10:46:43 +0800

    [fix]: warning in YYCategoriesMacro

--- YYCategories/YYCategoriesMacro.h
+++ YYCategories/YYCategoriesMacro.h
@@ -285,7 +285,7 @@ static inline dispatch_time_t dispatch_walltime_date(NSDate *date) {
 /**
  Whether in main queue/thread.
  */
-static inline bool dispatch_is_main_queue() {
+static inline bool dispatch_is_main_queue(void) {
     return pthread_main_np() != 0;
 }
 
